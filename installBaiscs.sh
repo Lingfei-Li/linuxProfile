b7b683406fa055880a511169e93691c2b8c1ff36
lingfei <cslilingfei@outlook.com>
2016-05-28 21:54:27 -0500

    added gitconfig to installation script

--- installBaiscs.sh
+++ installBaiscs.sh
@@ -1,2 +1,8 @@
+echo "Note: terminal will exit after installation"
 sudo apt install vim
 sudo apt-get install virtualbox-guest-dkms
+cp ./vimrc ~/.vimrc
+cp ./bashrc ~/.bashrc
+cp ./gitconfig ~/.gitconfig
+exit
+
